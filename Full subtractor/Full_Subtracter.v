d4ecbacbebc55da0624d758aaf6b675445caf521
SUPREETH <2KE22EC106@KLEIT.AC.IN>
2024-12-06 21:01:53 +0530

    Delete Full subtractor directory

--- Full subtractor/Full_Subtracter.v
+++
@@ -1,8 +0,0 @@
-module Full_Subtracter(A,B,Bin,Diff,Borrow);
-	input A,B,Bin;
-	output Diff,Borrow;
-	wire w1,w2,w3;
-	Half_Subtracter h1(A,B,w1,w2);
-	Half_Subtracter h2(w1,Bin,Diff,w3);
-	or (Borrow,w2,w3);
-endmodule
